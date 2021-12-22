7c1b7a8716bd1d7e81ffb7d058b1caf5e6557c19
César O. Araújo <oaraujocesar@gmail.com>
2021-12-22 20:58:33 -0300

    feat: :recycle: Add env variables

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import * as S from './styles'
 
 import { LogoProps } from 'types/api'
+import { getImageUrl } from 'utils/getImageUrl'
 
 const Logo = ({ data: { attributes } }: LogoProps) => {
   return (
     <S.LogoWrapper
-      src={`http://localhost:1337${attributes.url}`}
+      src={getImageUrl(attributes.url)}
       alt={attributes.alternativeText}
     />
   )
